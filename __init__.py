52cdecdc624d27acba16e6c5de0f4da61e8f2c02
Alan Barretto <alanbarretto@gmail.com>
2017-03-02 16:54:51 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+import bloghandler
+import blogfront
+import postcomments
+import deletecomments
+import deletepost
+import editcomments
+import frontpage
+import likepost
+import login 
+import logout
+import newpost 
+import postedit 
+import postpage 
+import register 
+import signup 
+import welcome 
+
+
