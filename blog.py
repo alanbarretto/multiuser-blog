d009c15ed2c166fb3f653aaf0412055cb2511ee5
Alan Barretto <alanbarretto@gmail.com>
2017-03-10 13:55:22 -0800

    refactor imports

--- blog.py
+++ blog.py
@@ -1,110 +1,20 @@
-from handlers import blogfront, bloghandler, comments, deletecomments, deletepost, editcomments, frontpage, likepost, login, logout, newpost, postedit, postpage, register, signup, welcome 
-                     
-from models import post, user, comment 
-import os
-import re
-import random
-import hashlib
-import hmac
-from string import letters
+#from handlers import blogfront, bloghandler, comments, \
+#                  deletecomments, deletepost, editcomments, frontpage, likepost,\
+#                  login, logout, newpost, postedit, postpage, register, signup, welcome 
 
-import webapp2
-import jinja2
+#from handlers import blogfront, frontpage, login, logout, newpost, \
+#                     register, signup, welcome, likepost, postcomments, \
+#                     deletecomments, deletepost, editcomments, postedit, \
+#                     postpage
 
-from google.appengine.ext import db
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
-
-secret = 'warriors'
-
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-def make_secure_val(val):
-    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
-
-def check_secure_val(secure_val):
-    val = secure_val.split('|')[0]
-    if secure_val == make_secure_val(val):
-        return val
-
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
-
-##### user stuff
-def make_salt(length = 5):
-    return ''.join(random.choice(letters) for x in xrange(length))
-
-def make_pw_hash(name, pw, salt = None):
-    if not salt:
-        salt = make_salt()
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s,%s' % (salt, h)
-
-#how to verify passwords
-def valid_pw(name, password, h):
-    salt = h.split(',')[0]
-    return h == make_pw_hash(name, password, salt)
-
-#This creates the ancestor element in the database to store all of our users
-def users_key(group = 'default'):
-    return db.Key.from_path('users', group)
-
-##### blog stuff
-
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
+from handlers import BlogHandler, BlogFront, FrontPage, Login, Logout, \
+                     NewPost, Register, Signup, Welcome, LikePost, PostComments, \
+                     DeleteComments, DeletePost, EditComments, PostEdit, \
+                     PostPage 
 
 
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-    return username and USER_RE.match(username)
 
-PASS_RE = re.compile(r"^.{3,20}$")
-def valid_password(password):
-    return password and PASS_RE.match(password)
 
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-def valid_email(email):
-    return not email or EMAIL_RE.match(email)
-
-class BlogHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        params['user'] = self.user
-        return render_str(template, **params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-    def set_secure_cookie(self, name, val):
-        cookie_val = make_secure_val(val)
-        self.response.headers.add_header(
-            'Set-Cookie',
-            '%s=%s; Path=/' % (name, cookie_val))
-
-    def read_secure_cookie(self, name):
-        cookie_val = self.request.cookies.get(name)
-        return cookie_val and check_secure_val(cookie_val)
-
-    def login(self, user):
-        self.set_secure_cookie('user_id', str(user.key().id()))
-
-    def logout(self):
-        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-
-    #checks to see if the user is logged in or not
-
-    def initialize(self, *a, **kw):
-        webapp2.RequestHandler.initialize(self, *a, **kw)
-        uid = self.read_secure_cookie('user_id')
-        self.user = uid and User.by_id(int(uid))
 
 
 
@@ -116,7 +26,7 @@ app = webapp2.WSGIApplication([('/', FrontPage),
                                ('/login', Login),
                                ('/blog/postedit/([0-9]+)', PostEdit),
                                ('/blog/deletepost/([0-9]+)', DeletePost),
-                               ('/blog/newcomment/([0-9]+)', Comments),
+                               ('/blog/newcomment/([0-9]+)', PostComments),
                                ('/blog/editcomment/([0-9]+)', EditComments),
                                ('/blog/deletecomment/([0-9]+)', DeleteComments),
                                ('/blog/like/([0-9]+)', LikePost),
