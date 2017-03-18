d6fde102bcfef4ff5a1bb6e1b3bdda68e3171fd1
Alan Barretto <alanbarretto@gmail.com>
2017-03-18 10:46:40 -0700

    Add files via upload

--- support/__init__.py
+++ support/__init__.py
@@ -1,3 +1,4 @@
 from support import render_str, make_secure_val, check_secure_val,\
                     render_post, make_salt, make_pw_hash, valid_pw,\
-                    valid_email, valid_username, users_key, blog_key
+                    valid_email, valid_username, users_key, blog_key,\
+                    valid_password
