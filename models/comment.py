9a44bde2c8bc5389e44a029eb6e2932d021fc01f
Alan Barretto <alanbarretto@gmail.com>
2017-03-20 22:18:32 -0700

    add comment_user to comment.py

--- models/comment.py
+++ models/comment.py
@@ -1,9 +1,11 @@
 from google.appengine.ext import db
 from support import render_str
+from models.user import User
 
 class Comment(db.Model):
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
+    comment_user = db.ReferenceProperty(User)
     creator = db.StringProperty()
     post_reference = db.StringProperty(required=True)
     user_Id = db.IntegerProperty()
